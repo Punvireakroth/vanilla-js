b2b9613a42f6fb3ff8e024784fa05779777cca6f
Punvireakroth <vireakrothpun@gmail.com>
2022-08-13 22:51:33 +0700

    fixing bugs

--- my_note/app.js
+++ my_note/app.js
@@ -1,7 +1,7 @@
-// Save note
+showNotes();
 
+// If user adds a note, save it to the localStorage
 let addBtn = document.getElementById("addBtn");
-
 addBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
   let notes = localStorage.getItem("notes");
@@ -16,23 +16,24 @@ addBtn.addEventListener("click", function (e) {
   showNotes();
 });
 
-// Display Note
+// Function to show elements from localStorage
 function showNotes() {
   let notes = localStorage.getItem("notes");
   if (notes == null) {
-    notesObj == [];
+    notesObj = [];
   } else {
-    notesObj == JSON.parse(notes);
+    notesObj = JSON.parse(notes);
   }
   let html = "";
   notesObj.forEach(function (element, index) {
-    html += `<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-                      <div class="card-body">
-                          <h5 class="card-title">Note ${index + 1}</h5>
-                          <p class="card-text"> ${element}</p>
-                          <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-danger">Delete Note</button> 
-                      </div>
-                  </div>`;
+    html += `
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <p class="card-text"> ${element}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-danger">Delete Note</button>
+                    </div>
+                </div>`;
   });
   let notesElm = document.getElementById("notes");
   if (notesObj.length != 0) {
@@ -40,4 +41,18 @@ function showNotes() {
   } else {
     notesElm.innerHTML = `Nothing to show! Create your first Note!`;
   }
+}
+
+// Function to delete a note
+function deleteNote(index) {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
 }
