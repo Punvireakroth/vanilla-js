aef1e5d3a807d76d2cfd0b67ee122b8d99817976
Punvireakroth <vireakrothpun@gmail.com>
2022-08-15 02:31:58 +0700

    added loader animation

--- quote_generator/script.js
+++ quote_generator/script.js
@@ -3,14 +3,27 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
-
+const loader = document.getElementById("loader");
 // Get Quote From API
 
 let apiQuote = [];
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // New quote
 
 function newQuote() {
+  loading();
   // pick random quote from API quote array
   const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)];
 
@@ -27,13 +40,15 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+  // Set Quote Hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -57,3 +72,4 @@ twitterBtn.addEventListener("click", tweetQuote);
 // On Load
 
 getQuotes();
+// loading();
