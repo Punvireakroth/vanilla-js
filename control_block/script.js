d41fd5b8c151aa0b414e95a6df19c1af3b9edbdb
Punvireakroth <vireakrothpun@gmail.com>
2022-12-23 11:53:49 +0700

    change the shape

--- control_block/script.js
+++ control_block/script.js
@@ -18,5 +18,27 @@ window.addEventListener('load', function(){
     size.addEventListener('input', function(){
         block.style.transform = `scale(${size.value})`;
     })
-      
+
+
+    //opacity of the block
+    let opacity = this.document.getElementById('opacity');
+    opacity.addEventListener('input', function(){
+        block.style.opacity = opacity.value;
+    })
+
+    //shape of the block
+    let okButton = document.getElementById('ok-shape');
+    let shape = document.getElementById('select');
+
+    okButton.addEventListener('click', function(){
+        if(shape.value == '1'){
+            block.style.borderRadius = '0';
+            block.style.transform = `rotate(0deg)`;
+        } else if(shape.value == '2'){
+            block.style.borderRadius = '50%';
+        }else if(shape.value === '3'){
+            block.style.borderRadius = '0';
+            block.style.transform = `rotate(45deg)`;
+        }
+    })
 })
