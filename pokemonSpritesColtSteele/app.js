4f454c50c241a0406ba564eb45d6e2b41cb5ee42
Punvireakroth <vireakrothpun@gmail.com>
2022-12-25 13:00:58 +0700

    generate more pokemon

--- pokemonSpritesColtSteele/app.js
+++ pokemonSpritesColtSteele/app.js
@@ -1,8 +1,11 @@
 const container = document.getElementById('container');
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const newImg = document.createElement('img');
 
-newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+for(let i = 1; i < 800; i++){
+    const newImg = document.createElement('img');
 
-document.body.appendChild(newImg);
+    newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+
+    container.appendChild(newImg);
+}
