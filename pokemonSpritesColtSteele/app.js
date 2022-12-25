7a8cde1dae75a1fc2f9517a22db28a82a2f938a7
Punvireakroth <vireakrothpun@gmail.com>
2022-12-25 13:14:04 +0700

    improve visual

--- pokemonSpritesColtSteele/app.js
+++ pokemonSpritesColtSteele/app.js
@@ -2,10 +2,19 @@ const container = document.getElementById('container');
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 
-for(let i = 1; i < 800; i++){
+for(let i = 1; i < 20; i++){
+    const pokemon = document.createElement('div');
+    const label = document.createElement('span');
     const newImg = document.createElement('img');
+    label.innerText = `${i}`;
 
     newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
 
-    container.appendChild(newImg);
+
+
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+
+    container.appendChild(pokemon);
+    
 }
