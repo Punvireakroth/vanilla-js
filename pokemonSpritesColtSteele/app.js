a25e816cf56d17a56cf242a51bc60515b2561d44
Punvireakroth <vireakrothpun@gmail.com>
2022-12-25 12:51:49 +0700

    initial commit

---
+++ pokemonSpritesColtSteele/app.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('container');
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const newImg = document.createElement('img');
+
+newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+
+document.body.appendChild(newImg);
